985a66772688308cd36e0ef0a9670b65b87b9aa1
Dieryson Araujo <115048301+Dieryson@users.noreply.github.com>
2023-02-15 13:43:52 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace DDDCidades
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o DDD da sua Cidade ");
+            int ddd = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (ddd)
+            {
+                case 61:
+                    Console.WriteLine("Brasilia");
+                    break;
+                case 71:
+                    Console.WriteLine("Salvador");
+                    break;
+                case 11:
+                    Console.WriteLine("Sao Paulo");
+                    break;
+                case 21:
+                    Console.WriteLine("Rio de Janeiro");
+                    break;
+                case 32:
+                    Console.WriteLine("Juiz de Fora");
+                    break;
+                case 19:
+                    Console.WriteLine("Campinas");
+                    break;
+                case 27:
+                    Console.WriteLine("Vitoria");
+                    break;
+                case 31:
+                    Console.WriteLine("Belo Horizonte");
+                    break;
+                default:
+                    Console.WriteLine("DDD nao cadastrado ou invalido ");
+                    break;
+            }
+        }
+
+    }
+
+}
